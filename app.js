2c87c8c761e42eba2eb51481039712ec91832660
Carla Babadopulos <cacababa@gmail.com>
2016-06-07 00:58:12 +0200

    Refactored again

--- app.js
+++ app.js
@@ -1,63 +1,54 @@
 var audio = new Audio("beep49.mp3");
-var timerOn = 0;
 var countBreaks = 0;
 var interval;
-function startTimer(duration, smallBreak, bigBreak, display) {
-  var timer = duration, minutes, seconds;
-  var cicles = duration + 1;
+var duration = 0;
+var wasBreak = false;
+
+function startClock(_duration) {
+  if(_duration) {
+    duration = _duration;
+  }
+  setClock(duration);
+
   interval = setInterval(function(){
-    minutes = parseInt(timer / 60, 10);
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = parseInt(timer % 60, 10);
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-    cicles -= 1;
-
-    if(--timer < 0) {
-      if (timer < 0 && cicles < 0){
-        timer = duration - duration;
-      } else {
-        timer = duration;
-      }
-    }
-
-    if(cicles <= 0) {
-      if(countBreaks > 4) {
-        clearInterval(interval);
-        countBreaks = 0;
-        audio.play();
-        startBreak(bigBreak, display);
-      } else {
-        clearInterval(interval);
-        audio.play();
-        startBreak(smallBreak, display);
-      }
-    }
-
-  }, 1000);
-}
-
-function startBreak(duration, display) {
-  var timer = duration, minutes, seconds;
-  interval = setInterval(function(){
-    minutes = parseInt(timer / 60, 10);
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = parseInt(timer % 60, 10);
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if(--timer < 0) {
-        timer = duration - duration;
+    setClock(duration);
+    if(duration <= 0) {
       clearInterval(interval);
-      start();
+      audio.play();
+      loadNextLoop();
     }
+    duration --;
   }, 1000);
 }
 
-function workingLoop(){
-  var userInput = document.getElementById("inputId").value || 25;
+function setClock(duration) {
+  var display = document.querySelector("#counter");
+  var minutes = parseInt(duration / 60, 10);
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var seconds = parseInt(duration % 60, 10);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  display.textContent = minutes + ":" + seconds;
+}
+
+function loadNextLoop() {
+  if(wasBreak) {
+    startClock(loopTime("workLoop"));
+  } else {
+    if(countBreaks >= 4){
+      countBreaks = 0;
+      startClock(loopTime("bigRest"));
+    } else {
+      countBreaks += 1;
+      startClock(loopTime("smallRest"));
+    }
+  }
+  wasBreak = !wasBreak;
+}
+
+function loopTime(type){
+  var defaultTime = {workLoop: 25, smallRest: 5, bigRest: 15}
+  var userInput = document.getElementById(type).value || defaultTime[type];
   if(userInput < 1){
     userInput *= 100;
   } else {
@@ -66,41 +57,21 @@ function workingLoop(){
   return userInput;
 }
 
-function smallBreak() {
-  var smallRest = document.getElementById("smallRest").value || 5;
-  if(smallRest < 1){
-    smallRest *= 100;
-  } else {
-    smallRest *= 60;
-  }
-  return smallRest;
-}
-
-function bigBreak() {
-  var bigRest = document.getElementById("bigRest").value || 15;
-  if(bigRest < 1){
-    bigRest *= 100;
-  } else {
-    bigRest *= 60;
-  }
-  return bigRest;
-}
-
 function pause(){
   clearTimeout(interval);
-  timerOn = 0;
+}
+
+function resume(){
+  startClock();
 }
 
 function stop(){
-  location.reload();
+  clearTimeout(interval);
+  wasBreak = false;
+  countBreaks = 0;
+  setClock(0);
 }
 
 function start(){
-  timerOn = 0;
-  countBreaks += 1;
-  var display = document.querySelector("#counter");
-  if (!timerOn){
-    timerOn = 1;
-    startTimer(workingLoop(), smallBreak(), bigBreak(), display);
-  }
+  startClock(loopTime("workLoop"));
 }
