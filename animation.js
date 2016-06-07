cf47cb1223dabcf192a623fc3f1f29a1b9fef19c
Carla Babadopulos <cacababa@gmail.com>
2016-06-07 18:51:30 +0200

    Hide/show functionality to buttons

--- animation.js
+++ animation.js
@@ -6,8 +6,15 @@ function removeSpinClass(element) {
   element.classList.remove('fa-spin', 'fa-fw');
 }
 
-function showHideForm(formId){
+function showHideForm(formId) {
   var _form = document.getElementById(formId);
   _form.style.display = (_form.style.display != "block") ? "block" : "none";
   return false;
+}
+
+function hideShow(element1, element2) {
+  document.getElementById(element1).classList.remove('show');
+  document.getElementById(element1).classList.add('hide');
+  document.getElementById(element2).classList.remove('hide');
+  document.getElementById(element2).classList.add('show');
 }
